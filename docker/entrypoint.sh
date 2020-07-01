1648c94e7f24306355e7e8abb27f735d363ea4c4
Lukasz Rzepa <lukasz.rzepa@whiteaster.com>
2020-07-01 21:27:35 +0200

    Update docker settings

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-/app/docker/wait_for.sh harvester_db:5432 -t 15 -- echo "Database (harvester_db) is up!"
-/app/docker/wait_for.sh harvester_mongo:27017 -t 15 -- echo "Database (harvester_mongo) is up!"
+/app/docker/wait_for.sh ce_db:5432 -t 2 -- echo "Database (ce_db) is up!"
+/app/docker/wait_for.sh ce_mongo:27017 -t 2 -- echo "Database (ce_mongo) is up!"
 
-cp /app/example.env /app/harvester/.env
+cp /app/example.env /app/collection_editor/.env
 cp /app/example.env /app/.env
 
 python /app/manage.py migrate
