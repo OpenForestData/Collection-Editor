9e602baaca449830836cba8b81e0724655ff58e5
Adrian Rupala <adrixop95@gmail.com>
2020-07-21 11:25:30 +0200

    cleanup, remove runserver configurations, add collectstatic in entrypoint

--- docker/entrypoint_prod.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-/app/docker/wait_for.sh ce_db:5432 -t 2 -- echo "Database (ce_db) is up!"
-/app/docker/wait_for.sh ce_mongo:27017 -t 2 -- echo "Database (ce_mongo) is up!"
-
-python /app/manage.py migrate
-python /app/manage.py loaddata initial_groups.json
-/usr/local/bin/gunicorn collection_editor.wsgi -b 0.0.0.0:8000
