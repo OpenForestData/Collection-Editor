01d9d573387e0bca7d7c74de2c6e002a95550df7
Lukasz Rzepa <lukasz.rzepa@whiteaster.com>
2020-07-29 10:33:13 +0200

    Additional data in history serializer

--- core/serializers/datatable_action.py
+++ core/serializers/datatable_action.py
@@ -8,7 +8,14 @@ class DatatableActionReadOnlySerializer(serializers.ModelSerializer):
     DatatableAction serializer for read-write operations
     """
 
+    username = serializers.SerializerMethodField()
+    datatable_title = serializers.CharField(source="datatable.title")
+
     class Meta:
         model = DatatableAction
         fields = '__all__'
         read_only_fields = [fields]
+
+    def get_username(self, obj):
+        full_name = f'{obj.user.first_name} {obj.user.last_name}'
+        return full_name if full_name.strip() else obj.user.username
